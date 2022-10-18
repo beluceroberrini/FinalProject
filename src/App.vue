858efa23ce5b8445ae8d4adbafbd50ecbafae535
Maria Belen <belul@hotmail.com>
2022-10-18 12:51:06 +0200

    supabase

--- src/App.vue
+++ src/App.vue
@@ -11,8 +11,10 @@
 <router-view></router-view>
 </template>
 <script setup>
-import Home from "./views/Home.vue";
-import Login from "./views/Login.vue";
+import { createClient } from '@supabase/supabase-js'
+
+const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_KEY)
+console.log(supabase)
 
 console.log(process.env.NODE_ENV, import.meta.env.BASE_URL)
 
