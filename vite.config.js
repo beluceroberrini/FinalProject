df9decae0c89c454944b4441b98d63bac97da854
Maria Belen <belul@hotmail.com>
2022-10-27 10:00:00 +0200

    Pages

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,22 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-
-  //carpeta donde creara el build
-  build: {outDir: 'docs'},
-  //comprobamos si esstamos en produccion y la base es el nombre del repositorio, sino  
-  //condicion ? 'nombre-repositorio' : '/'
-  //if(process.env.NODE_ENV === 'produccion'){
-  //}base: 'produccion'{
-  //     }else{
-  //    base: '/'
-  //} 
-  //Produccion seria github pages
-  // desarrollo seria yarn dev
-
-  base: process.env.NODE_ENV === 'production' ? '/FinalProject/' : '/'
+  base: process.env.NODE_ENV === 'production' ? '/FinalProject/' : '/',
+  build: {
+      outDir: 'docs',
+      rollupOptions: {
+          output: {
+              entryFileNames: `assets/file_[name].[hash].js`,
+              chunkFileNames: `assets/file_[name].[hash].js`,
+              assetFileNames: `assets/file_[name].[hash].[ext]`,
+          },
+      }
+  },
+  server: {
+      base: process.env.NODE_ENV === 'production' ? '/FinalProject/' : '/',
+  }
 })
